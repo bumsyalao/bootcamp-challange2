e69315b8ae0a5943347cc0ae5b9b4e81752580a4
bumsyalao <alaobunmi93@gmail.com>
2017-07-04 17:47:48 +0100

    final draft

--- src/main.js
+++ src/main.js
@@ -1,73 +1,46 @@
-
+"use strict";
 class shoppingcart {
-	
-	constructor (itemName, quantity, price)
-	{
-	this.itemName = itemName;
-	this.quantity = quantity;
-	this.price = price;
-	//this.total = total;
-	let total = quantity * price;
-	}
-
-	toString () 
-	{
-	return 'Your Reciept: item:' ${this.item} '|Quantity: '${this.quantity}'| Price: '${this.price}' | Total: '${total}
-	}
-
-	print ()
-	{
-		console.log( this.toString() );
-	}
-
-	calcTotal ()
-	{
-		total = quantity * price;
-	}
-
-	
-	
-	
-}
-
+    constructor (itemName, quantity, price){
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+        this.total = this.quantity * this.price;
+    }// end of constructor function
+    toString () {
+    return 'Your Reciept: item: ${this.item} |Quantity: ${this.quantity}  Price: ${this.price} | Total: ${total}';
+    }//end of function toString
+    print (){
+        console.log( toString() );
+    }//end of function print
+    calcTotal (){
+        this.total = this.quantity * this.price;
+    }//end of function calcTotal
+    
+}//end of class shoppingcart
 class shop extends shoppingcart {
-
-	// constructor(quantity)
-	// {
-	// 	this.quantity = quantity;
-	// 	quantity = 100
-	// }
-
-	removeItem()
-	{
-
-		quantity --
-	}
-
-}
-
+     constructor(quantity){
+        super(quantity);
+        super.this.quantity = 100
+    }//end of constructor
+    removeItem(){
+        this.quantity = this.quantity --
+    }//end of function removeItem
+}//end of subclass shop
 class customer extends shoppingcart {
-
-	constructor(balance, cashPaid)
-	{
-		this.balance = balance;
-		this.cashPaid = cashPaid;
-		balance = 0
-	}
-	
-	checkout()
-
-	{	
-		if (cashPaid < total)
-		{
-			return "Cash paid not enough";
-		}
-
-		balance = cashPaid - total;
-		return balance;
-	}
-}
-
+    constructor(balance, cashPaid){
+        this.balance = balance;
+        this.cashPaid = cashPaid;
+        this.balance = 0
+    }//end of constructor function
+  
+    checkout(){    
+        if (this.cashPaid < super.this.total){
+            return "Cash paid not enough";
+        }//end of if statement
+        this.balance = his.cashPaid - super.this.total;
+        return this.balance;
+    }//end of function checkout
+}//end of subcalss customer
 const bunmi = new shoppingcart('rice', 1, 200);
 bunmi.print();
 console.log(bunmi.calcTotal);
